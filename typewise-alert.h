066a56169feb8ae856e89a543142216d17763cce
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-01-09 16:02:40 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -25,7 +25,7 @@ typedef struct {
 typedef struct{
   int lowerLimit;
   int upperLimit;
-}coolingLimitType;
+} coolingLimitType;
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
