ef4bbc91ad27bcf67db5d0774514257404dab07d
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-01-12 15:43:51 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -119,3 +119,23 @@ TEST_CASE("send email alert low -> TO_EMAIL HI_ACTIVE_COOLING inferBreach:NORMAL
   batteryCharacter1.coolingType = HI_ACTIVE_COOLING;
   checkAndAlert(TO_EMAIL, batteryCharacter1, 0);
 }
+
+/* TO_CONTROLLER HI_ACTIVE_COOLING inferBreach:TOO_HIGH sendToEmail:TOO_HIGH */
+TEST_CASE("To controller alert high -> TO_CONTROLLER HI_ACTIVE_COOLING inferBreach:TOO_HIGH sendToEmail:TOO_HIGH")
+{
+  batteryCharacter1.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryCharacter1, 36);
+  //REQUIRE(checkAndAlert(TO_EMAIL, batteryCharacter1, 36));
+}
+/* TO_CONTROLLER HI_ACTIVE_COOLING inferBreach:TOO_LOW sendToEmail:TOO_LOW */
+TEST_CASE("To controller alert low -> TO_CONTROLLER HI_ACTIVE_COOLING inferBreach:TOO_LOW sendToEmail:TOO_LOW")
+{
+  batteryCharacter1.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryCharacter1, -1);
+}
+/* TO_CONTROLLER HI_ACTIVE_COOLING inferBreach:NORMAL sendToEmail:NORMAL */
+TEST_CASE("To controller alert low -> TO_CONTROLLER HI_ACTIVE_COOLING inferBreach:NORMAL sendToEmail:NORMAL")
+{
+  batteryCharacter1.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryCharacter1, 0);
+}
