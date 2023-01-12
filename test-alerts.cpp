1248794535d0fda0c5a0cf8858297df2ad357924
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-01-12 12:04:36 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,30 +3,21 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits")
+{
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
   /* InferBreach testcases */
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits")
+{
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
 }
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits")
+{
   REQUIRE(inferBreach(20, 20, 20) == NORMAL);
 }
   /* classifyTemperatureBreach testcases */
-/*
+TEST_CASE("classifyTemperatureBreach")
+{
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 12) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 12) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 12) == NORMAL);
-  
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 41) == TOO_HIGH);
-  /*
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 12) == NORMAL);
-  */
-  /* sendToController testcases 
-  REQUIRE(sendToController(NORMAL));
-  REQUIRE(sendToController(TOO_LOW));
-  REQUIRE(sendToController(TOO_HIGH));
-  */
 }
