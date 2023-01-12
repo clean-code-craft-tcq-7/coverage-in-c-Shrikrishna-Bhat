3f11e768a99618d9f9fe80adefceb980ae68bda2
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-01-12 14:29:59 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -46,20 +46,18 @@ TEST_CASE("classifyTemperatureBreach MED_ACTIVE_COOLING NORMAL")
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 12) == NORMAL);
 }
   /* sendToController testcases */
-/*
 TEST_CASE("sendToController NORMAL")
 {
-  REQUIRE(sendToController(NORMAL));
+  sendToController(NORMAL);
 }
 TEST_CASE("sendToController TOO_LOW")
 {
-  REQUIRE(sendToController(TOO_LOW));
+  sendToController(TOO_LOW);
 }
 TEST_CASE("sendToController TOO_HIGH")
 {
-  REQUIRE(sendToController(TOO_HIGH));
+  sendToController(TOO_HIGH);
 }
-*/
 
 /* checkAndAlert testcases */
 TEST_CASE("send email alert high")
