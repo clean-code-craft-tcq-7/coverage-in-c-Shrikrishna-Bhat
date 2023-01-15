05d8ed3d0f1949f5e330a169de2ed1d23c784f99
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-01-15 15:52:52 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -22,7 +22,7 @@ static coolingLimitType TypesOfLimitCooling(CoolingType coolingType)
   coolingLimitTypeReturn.upperLimit = 0;
   
   //(coolingType == CoolingType_en == 0) ? coolingLimitTypeReturn.upperLimit = 35 : ((coolingType == CoolingType_en == 1) ? coolingLimitTypeReturn.upperLimit = 45 : coolingLimitTypeReturn.upperLimit = 40);
-  if (coolingType == CoolingType_en == 0)
+  if (coolingType == PASSIVE_COOLING == 0)
   {
     coolingLimitTypeReturn.upperLimit = 35;
   }
